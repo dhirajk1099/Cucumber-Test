f95557678b23985623c499b630b85c1e68283a8d
dhiraj shukla <dhirajshuklalk@gmail.com>
2020-04-14 09:50:49 +0530

    created Mthods for feature lines and also executed scenario-outline

--- src/test/Features/Test.feature
+++ src/test/Features/Test.feature
@@ -1,7 +1,21 @@
 Feature: Cucumber Automation
 
-Scenario: First Scenario
-Given I open the application
-Then I enter username
-And I enter password
-And I click on submit button
+  Scenario: First Scenario
+    Given I open the application
+    Then I enter username "ab"
+    And I enter password ba
+    And I click on submit button
+
+  Scenario Outline: First Scenario
+    Given I open the application
+    Then I enter username "<Username>"
+    And I enter password <Password>
+    And I click on submit button
+
+    Examples: 
+      | Username | Password |
+      | ab       | ba       |
+      | ab       | ba       |
+      | ab       | ba       |
+      | ab       | ba       |
+      | ab       | ba       |
--- src/test/java/StepDefinition/LoginPageStepDefinition.java
+++ src/test/java/StepDefinition/LoginPageStepDefinition.java
@@ -1,8 +1,13 @@
 package StepDefinition;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -10,31 +15,48 @@ import io.cucumber.java.en.Then;
 public class LoginPageStepDefinition {
 
 	WebDriver driver = null;
-@Given("I open the application")
-public void openApplication() {
-	
-	
-	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+""+"/libraries/chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://www.myntra.com/");
-}
-	
-@Then("I enter username")
-public void enterUsername() {
-	System.out.println("Enter username");
-}
 
-@And("I enter password")
-public void enterPassword() {
-	System.out.println("Enter password");
-}
-
-@Then("I click on submit button")
-public void clcikSubmitBtn() {
-	System.out.println("Click submit button");
-	driver.close();
-	driver.quit();
+	@Before
+	public void beforeScenario() {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "" + "/libraries/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 	
-}
+	@After
+	public void afterScenario() {
+		driver.manage().deleteAllCookies();
+		driver.close();
+		driver.quit();
+	}
+
+	@Given("I open the application")
+	public void openApplication() {
+
+		driver.get("https://www.amazon.com/");
+	}
+
+	@Then("^I enter username \"([^\"]*)\"$")
+	public void enterUsername(String username) {
+
+		driver.findElement(By.xpath("//a[@id='nav-link-accountList']")).click();
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+
+	@And("^I enter password ([^\"]*)$")
+	public void enterPassword(String password) {
+
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+
+	}
+
+	@Then("I click on submit button")
+	public void clcikSubmitBtn() {
+		driver.findElement(By.xpath("//input[@id='signInSubmit']")).click();
+		
+
+	}
 }
